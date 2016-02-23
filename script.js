33b13c22605926b69d4ec365056bc0ef0c5cce40
lvlacmep <lex-ll@mail.ru>
2016-02-23 14:33:02 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
 $(document).ready(function(){
-    jQuery.expr[":"].contains = function( elem, i, match, array ) {
-    return (elem.textContent || elem.innerText || jQuery.text( elem ) || "").toLowerCase().indexOf(match[3].toLowerCase()) >= 0;}
-    var table=$('.table-class').html();
-    $('#btn').click(function(){ 
-	if ($('#field').val() == ''){alert('Пустое поле');} 
-	else{
-    $(".table").append('<table class="class" id="mytable">'+table+'</table>');
-		$('table.table-class').remove();
-		var s1 = $('#field').val();
-        if($('#mytable .search:contains("'+s1+'")').length>0){
-    $('#mytable .search:contains("'+s1+'")').each(function(){ 
-        var s2=$(this).html();
-	    $('<tr class="search">'+s2+'</tr>').insertAfter($("tr:first"));
-  })
-     }
-    else{alert('Ничего не найдено');}
-    $('#mytable tr#del').remove(':contains("'+s1+'")');
-    $("table").attr("class", "table-class")
-    $('input').val('');}
-	})    			 
+	$('#btn').click(function(){
+		var tbl = $('#mytable').html();
+		var srch = $('#field').val();
+		$('#mytable tr:not(:first)').each(function(){
+			if(!$(this).find('td').is(':contains("' + srch + '")'))
+			{
+				$('<tr">'+$(this).html()+'</tr>').insertAfter($("tr:last"));
+				$(this).remove();
+			}
+		});
+		
+	});
 });
